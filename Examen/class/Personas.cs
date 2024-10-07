aa69b3e5592c7c2b98603d25ec35475bc7263c9a
Jorge Elias Concepcion Lorenzo <111101748+george23elias@users.noreply.github.com>
2024-10-06 20:48:55 -0400

    Error.

--- Examen/class/Personas.cs
+++
@@ -1,8 +0,0 @@
-public class Person
-{
-    public string Nombre { get; set; }   // Nombre de la persona
-    public string Apellido { get; set; }  // Apellido de la persona
-    public TimeSpan Hora { get; set; }    // Hora como TimeSpan
-    public DateTime Fecha { get; set; }   // Fecha en formato DateTime
-    public string Estado { get; set; }     // Estado de la persona
-}
